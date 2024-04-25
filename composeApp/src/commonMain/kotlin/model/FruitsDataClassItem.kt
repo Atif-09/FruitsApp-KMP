e152954d6184750915e069d744f08d5092eb4ed7
Muhammad Atif <ma2669637@gmail.com>
2024-04-25 11:52:24 +0500

    added serialize to model class

--- composeApp/src/commonMain/kotlin/model/FruitsDataClassItem.kt
+++ composeApp/src/commonMain/kotlin/model/FruitsDataClassItem.kt
@@ -1,18 +1,34 @@
 package model
 
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class FruitsDataClassItem(
+    @SerialName("family")
     val family: String,
+    @SerialName("genus")
     val genus: String,
+    @SerialName("id")
     val id: Int,
+    @SerialName("name")
     val name: String,
+    @SerialName("nutritions")
     val nutritions: Nutritions,
+    @SerialName("order")
     val order: String
 )
 
+@Serializable
 data class Nutritions(
+    @SerialName("calories")
     val calories: Int,
+    @SerialName("carbohydrates")
     val carbohydrates: Double,
+    @SerialName("fat")
     val fat: Double,
-    val protein: Int,
+    @SerialName("protein")
+    val protein: Double,
+    @SerialName("sugar")
     val sugar: Double
 )
