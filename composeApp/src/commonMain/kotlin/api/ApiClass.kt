eb82e9e443487d2aa8793661432894e39988f0f0
Muhammad Atif <ma2669637@gmail.com>
2024-04-25 11:15:32 +0500

    added api class

---
+++ composeApp/src/commonMain/kotlin/api/ApiClass.kt
@@ -0,0 +1,28 @@
+package api
+
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.request.get
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
+
+
+class ApiClass {
+
+    private val client = HttpClient {
+        install(ContentNegotiation) {
+            json(Json {
+                ignoreUnknownKeys = true
+                useAlternativeNames = false
+            })
+        }
+    }
+
+   suspend fun githubUsers(){
+        val response = client.get("https://www.fruityvice.com/api/fruit/all")
+
+    }
+
+
+}
